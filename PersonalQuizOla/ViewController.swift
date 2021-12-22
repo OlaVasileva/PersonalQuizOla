19d90e423db6c8893e10bc38c043cd2dbced42f5
OlaVasileva <vasileva_ola@mail.ru>
2021-12-22 14:26:06 +0300

    Initial Commit

---
+++ PersonalQuizOla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizOla
+//
+//  Created by Васильева Ольга on 22.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
