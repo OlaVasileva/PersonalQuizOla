4f50e9f77fb279f24a868cf96f93d15fa447c3db
OlaVasileva <vasileva_ola@mail.ru>
2021-12-22 23:41:14 +0300

    with mistakes

---
+++ PersonalQuizOla/ViewControllers/IntroViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IntroViewController.swift
+//  PersonalQuizOla
+//
+//  Created by Васильева Ольга on 22.12.2021.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    //MARK: - IB Actions
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {}
+
+}
